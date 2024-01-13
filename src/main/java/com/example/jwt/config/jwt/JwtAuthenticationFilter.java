ceac2522f8fd076e4e2f5a720ffb3bf0eb7c1fcb
chanhee01 <joch4267@naver.com>
2024-01-13 16:23:57 +0900

    jwt를 이용한 로그인

--- src/main/java/com/example/jwt/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/jwt/config/jwt/JwtAuthenticationFilter.java
@@ -1,12 +1,20 @@
 package com.example.jwt.config.jwt;
 
+import com.example.jwt.config.auth.PrincipalDetails;
+import com.example.jwt.config.dto.LoginRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 // 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter가 있는데, /login 요청해서 username, password를 post로 전송하면
 // 이 필터가 동작을 한다.
@@ -18,7 +26,40 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
-        System.out.println("JwtAuthenticationFilter : 로그인 시도중");
-        return super.attemptAuthentication(request, response);
+        System.out.println("JwtAuthenticationFilter : 진입");
+
+        ObjectMapper om = new ObjectMapper();
+        LoginRequestDto loginRequestDto = null;
+        try {
+            loginRequestDto = om.readValue(request.getInputStream(), LoginRequestDto.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("JwtAuthenticationFilter : " + loginRequestDto);
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
+        // user의 정보를 가지고 토큰을 만든다.
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+        // authenticationManager를 만들어서 위에서 생성한 토큰을 넣는다.
+        // 이게 실행될 때 PrincipalDetailsService의 loadUserByUsername() 함수가 실행됨
+
+        PrincipalDetails principalDetailis = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("Authentication : " + principalDetailis.getUser().getUsername());
+        // 인증이 정상적으로 되어서 authentication 객체가 session 영역에 저장됨.
+        // session 영역에 있는 authentication의 principal 객체가 출력된다는건 -> 로그인이 된다는 것이다.
+        return authentication;
+    }
+
+    // attemptAuthentication 실행 후 인증이 정상적으로 되었으면 successfulAuthentication 함수가 실행된다.
+    // 여기서 JWT 토큰을 만들어서 request 요청한 사용자에게 JWT 토큰을 response 하면 된다.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        System.out.println("successfulAuthentication 실행 : 인증이 완료됨 ");
+
     }
 }
